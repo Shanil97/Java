d86b3005472d56150701c4d3ba1d61796593eac9
Shanil97 <46552983+Shanil97@users.noreply.github.com>
2021-05-30 19:32:59 +0530

    Add files via upload

---
+++ StringSort/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String []args){
+        String[] actualNames = new String[]{
+                "Johnson", "Wilson",
+                "Wilkinson", "Abraham", "Dagobert"
+        };
+        final Sort sorter = new Sort();
+        sorter.sort(actualNames);
+        for (final String name : actualNames) {
+            System.out.println(name);
+        }
+    }
+}
---
+++ StringSort/src/Sort.java
@@ -0,0 +1,15 @@
+public class Sort {
+    public void sort(String[] names){
+        int n = names.length;
+        while(n > 1){
+            for(int i = 0; i < n - 1; i++){
+                if(names[i].compareTo(names[i + 1]) > 0){
+                    final String tmp = names[i + 1];
+                    names[i + 1] = names[i];
+                    names[i] = tmp;
+                }
+            }
+            n--;
+        }
+    }
+}
